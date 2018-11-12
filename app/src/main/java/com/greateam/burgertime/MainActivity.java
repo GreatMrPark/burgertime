96dbff6ca52ecf35338fbc28b1564d8c2a73c3c5
Great Mr Park <greatmrpark@gmail.com>
2018-11-12 22:29:02 +0900

    #2228 Carousel

--- app/src/main/java/com/greateam/burgertime/MainActivity.java
+++ app/src/main/java/com/greateam/burgertime/MainActivity.java
@@ -1,11 +1,18 @@
 package com.greateam.burgertime;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.greateam.burgertime.common.log.Dlog;
@@ -13,6 +20,9 @@ import com.greateam.burgertime.common.log.Dlog;
 public class MainActivity extends AppCompatActivity {
 
     EditText editUrl;
+    Button closePopupBtn;
+    PopupWindow popupWindow;
+    LinearLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +72,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),DataBaseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mainLayout = findViewById(R.id.mainLayout);
+
+        Button buttonPopup = findViewById(R.id.buttonPopup); /*페이지 전환버튼*/
+        buttonPopup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //instantiate the popup.xml layout file
+                LayoutInflater layoutInflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View customView = layoutInflater.inflate(R.layout.activity_popup,null);
+
+                closePopupBtn = customView.findViewById(R.id.closePopupBtn);
+
+                //instantiate popup window
+                popupWindow = new PopupWindow(customView, ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+                //display the popup window
+                popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
+
+                //close the popup window on button click
+                closePopupBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
+            }
+        });
+
+        Button fullscreenPopup = findViewById(R.id.fullscreenPopup); /*페이지 전환버튼*/
+        fullscreenPopup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),FullscreenActivity.class);
                 startActivity(intent);
             }
         });
